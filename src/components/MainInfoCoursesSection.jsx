dfec5a953f2443f0b3070fd1ee35d4de49ee3b69
impooya <samarkarpooya@gmail.com>
2024-09-28 15:53:18 +0330

    update course dtails side bar

--- src/components/MainInfoCoursesSection.jsx
+++ src/components/MainInfoCoursesSection.jsx
@@ -46,9 +46,9 @@ function MainInfoCoursesSection({ leftContent, rightContent }) {
         <div
           className={`${
             courseDetailsConfig.openCourseDetails
-              ? "scale-in-top "
-              : "scale-out-top"
-          }   w-[300px]  h-[650px] bg-white fixed top-0 z-[999] left-0 right-0 bottom-0 my-auto mx-auto rounded-xl `}
+              ? "visible opacity-100 scale-in-top "
+              : "opacity-0 invisible scale-out-top"
+          }  transition-all duration-700 w-[300px]  h-[650px] bg-white fixed top-0 z-[999] left-0 right-0 bottom-0 my-auto mx-auto rounded-xl `}
         >
           <div className="courses-info  overflow-scroll h-[600px]">
             {leftContent}
