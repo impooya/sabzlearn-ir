f185346017ae81975f32fc2e6afc61c4e7952e83
impooya <samarkarpooya@gmail.com>
2024-10-05 22:25:38 +0330

    manage inputs stats from form with use reducer

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,18 +1,47 @@
+import { useReducer } from "react";
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE": {
+      return {
+        ...state,
+        value: action.value,
+        isValid: action.isValid,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
 function Inputs({ mainElement, type, placeholder, className }) {
+  const [mainInput, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+  });
   const onChangeHandler = (event) => {
     console.log(event.target.value);
+    dispatch({
+      type: "CHANGE",
+      value: event.target.value,
+      isValid: true,
+    });
   };
   const element =
     mainElement === "input" ? (
       <input
         type={type}
+        value={mainInput.value}
         placeholder={placeholder}
-        className={className}
+        className={`${className} ${
+          mainInput.isValid ? "border-green-primery" : "border-red-500"
+        }`}
         onChange={onChangeHandler}
       />
     ) : (
       <textarea
         placeholder={placeholder}
+        value={mainInput.value}
         className={className}
         onChange={onChangeHandler}
       />
