56896bf0b9cc4c8d1053c6bf08a2f12042754abe
impooya <samarkarpooya@gmail.com>
2024-11-19 13:36:03 +0330

    finish sabz learn project

--- src/components/PAdminPrivate.jsx
+++ src/components/PAdminPrivate.jsx
@@ -1,20 +1,13 @@
 import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { AuthContext } from "../contexts/authContext"; // Adjust the import based on your file structure
 
 function PAdminPrivate({ children }) {
   const { isLoggedIn } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate("/");
-    }
-  }, [isLoggedIn, navigate]);
 
   // Render children only if the user is not logged in
-  return !isLoggedIn ? <>{children}</> : null;
+  return !isLoggedIn ? <>{children}</> : <Navigate to={"/"} />;
 }
 
 PAdminPrivate.propTypes = {
